69c4fd785423cff369ae1e9003089d012bde43d4
gafiirfandi <gafiirfandi45@gmail.com>
2020-04-16 15:15:24 +0700

    building the structure of the project

---
+++ MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/MyAcApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.MyAc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAcApplication.class, args);
+    }
+
+}
