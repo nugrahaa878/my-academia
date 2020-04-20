f1224f8f40937884dc3f7f8857c86c26c41d3edd
Milhamag <milhamag@gmail.com>
2020-04-20 22:37:56 +0700

    membuat forumController yang baru menampilkan halaman utama forum, serta membuat html nya

--- MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/controller/ForumController.java
+++ MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/controller/ForumController.java
@@ -1,11 +1,27 @@
 package id.ac.ui.cs.advprog.MyAc.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import id.ac.ui.cs.advprog.MyAc.model.Post;
+import id.ac.ui.cs.advprog.MyAc.service.ForumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping(path = "/api/matkul")
+import java.util.List;
+
+@Controller
 public class ForumController {
+    @Autowired
+    private ForumService forumService;
+
+    @RequestMapping("/forum")
+    public String viewForumPage(Model model) {
+        List<Post> postList = forumService.findAll();
+        model.addAttribute("postList", postList);
+
+        return "halamanForum";
+    }
 
 }
