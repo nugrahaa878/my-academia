0c330283fa064703872ceeb1f7d7046b2894b902
Milhamag <milhamag@gmail.com>
2020-04-20 22:13:40 +0700

    membuat test untuk kelas ForumServiceImpl

---
+++ MyAc/src/test/java/id/ac/ui/cs/advprog/MyAc/service/ForumServiceImplTest.java
@@ -0,0 +1,62 @@
+package id.ac.ui.cs.advprog.MyAc.service;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import id.ac.ui.cs.advprog.MyAc.model.Post;
+import id.ac.ui.cs.advprog.MyAc.repository.ForumRepository;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ForumServiceImplTest {
+    @Mock
+    private ForumRepository forumRepository;
+
+    @InjectMocks
+    private ForumServiceImpl forumService;
+
+    private Post post;
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.post = new Post(1, "judul post", "isi post", "topik matkul");
+    }
+
+    @Test
+    public void whenFindAllIsCalledItShouldCallForumRepositoryFindAll() {
+        forumService.findAll();
+
+        verify(forumRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void whenGetIsCalledItShouldCallForumRepositoryFindById() {
+        forumService.get((long) 1);
+
+        verify(forumRepository, times(1)).findById((long) 1);
+    }
+
+    @Test
+    public void whenDeleteIsCalledItShouldCallForumRepositoryDeleteById() {
+        forumService.delete((long) 1);
+
+        verify(forumRepository, times(1)).deleteById((long) 1);
+    }
+
+
+
+    @Test
+    public void whenSaveIsCalledItShouldCallForumRepositorySave() {
+
+        forumService.save(post);
+
+        verify(forumRepository, times(1)).save(post);
+    }
+}
