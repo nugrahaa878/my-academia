f33199b192514d0f01ab9e517de7bc5a5ce1e040
Ari Nugraha <nugrahaa878@gmail.com>
2020-05-01 02:04:38 -0700

    [RED] add test for userregistrationdto

---
+++ MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/dto/UserRegistrationDto.java
@@ -0,0 +1,48 @@
+package id.ac.ui.cs.advprog.MyAc.dto;
+
+import id.ac.ui.cs.advprog.MyAc.constraint.FieldMatch;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@FieldMatch.List({
+        @FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match"),
+        @FieldMatch(first = "email", second = "confirmEmail", message = "The email fields must match")
+})
+
+public class UserRegistrationDto {
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String confirmPassword;
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @Email
+    @NotEmpty
+    private String confirmEmail;
+
+    @AssertTrue
+    private Boolean terms;
+
+    public String getFirstName() {
+        return null;
+    }
+
+    public void setFirstName(String firstName) {
+
+    }
+
+
+}
