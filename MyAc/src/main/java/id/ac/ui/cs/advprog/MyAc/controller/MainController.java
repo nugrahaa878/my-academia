dcb5f5c6d5ec8cf59c743da0bfc22f613850c348
Ari Nugraha <nugrahaa878@gmail.com>
2020-05-14 06:57:45 -0700

    fix bug - user name in home

--- MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/controller/MainController.java
+++ MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/controller/MainController.java
@@ -1,11 +1,20 @@
 package id.ac.ui.cs.advprog.MyAc.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import id.ac.ui.cs.advprog.MyAc.model.User;
+import id.ac.ui.cs.advprog.MyAc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class MainController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/")
     public String root() {
@@ -23,7 +32,9 @@ public class MainController {
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Principal principal, Model model) {
+        User user = userRepository.findByEmail(principal.getName());
+        model.addAttribute("nama", user.getFirstName());
         return "index";
     }
 
