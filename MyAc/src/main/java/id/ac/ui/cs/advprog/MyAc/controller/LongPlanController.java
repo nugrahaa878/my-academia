07baf9c8e53f22e39667e1d534a6fc5a17df9458
Ali Irsyaad Nursyaban <ali.irsyaad@ui.ac.id>
2020-05-14 13:49:47 +0700

    [RED] Testing for LongPlanController

---
+++ MyAc/src/main/java/id/ac/ui/cs/advprog/MyAc/controller/LongPlanController.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.MyAc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/longplan")
+public class LongPlanController {
+
+
+}
