f76dc3df43b9b15ff65ecce99a6b88cf1819f266
Ari Nugraha <nugrahaa878@gmail.com>
2020-05-15 01:42:31 -0700

    fix test

--- MyAc/src/test/java/id/ac/ui/cs/advprog/MyAc/MyAcApplicationTest.java
+++ MyAc/src/test/java/id/ac/ui/cs/advprog/MyAc/MyAcApplicationTest.java
@@ -1,10 +1,14 @@
 package id.ac.ui.cs.advprog.MyAc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class MyAcApplicationTest {
+
     @Test
-    public void main() {
-        MyAcApplication.main(new String[] {});
+    void contextLoads() {
+        
     }
+
 }
