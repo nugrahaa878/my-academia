1d46a40247479c1acda607288c22906720c92768
Ari Nugraha <nugrahaa878@gmail.com>
2020-05-15 01:47:31 -0700

    fix test

--- MyAc/src/test/java/id/ac/ui/cs/advprog/MyAc/MyAcApplicationTest.java
+++ MyAc/src/test/java/id/ac/ui/cs/advprog/MyAc/MyAcApplicationTest.java
@@ -5,10 +5,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MyAcApplicationTest {
-
-    @Test
-    void contextLoads() {
-        
-    }
-
+    
 }
